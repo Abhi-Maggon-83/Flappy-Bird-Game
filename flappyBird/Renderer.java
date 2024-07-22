e48794ca1ab9b00c77a25f8e22dc7e1e53f3a5a0
Abhi Maggon <110176348+Abhi-Maggon-83@users.noreply.github.com>
2024-07-22 21:51:37 +0530

    Update Renderer.java

--- flappyBird/Renderer.java
+++ flappyBird/Renderer.java
@@ -1,20 +1,18 @@
 package flappyBird;
 
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 
-public class Renderer extends JPanel
-{
+public class Renderer extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void paintComponent(Graphics g)
-	{
+	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		FlappyBird.flappyBird.repaint(g);
+		if (FlappyBird.flappyBird != null) {
+			FlappyBird.flappyBird.repaint(g);
+		}
 	}
-	
 }
